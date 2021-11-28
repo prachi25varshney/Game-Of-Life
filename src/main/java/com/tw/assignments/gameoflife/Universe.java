53506e34115c922ac12ae56a2028f39f45aa8f50
Prachi Varshney <prachivarshneythoughtworks.com@Prachis-MBP.bbrouter>
2021-11-29 00:04:32 +0530

    [Prachi] Add. Logic to remove Cells which are dead by loneliness(When live neighbours are less than 2)

--- src/main/java/com/tw/assignments/gameoflife/Universe.java
+++ src/main/java/com/tw/assignments/gameoflife/Universe.java
@@ -4,15 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Universe {
-    private List<Cell> currentGeneration;
+    private final List<Cell> currentGeneration;
 
     public Universe(List<Cell> currentGeneration) {
         this.currentGeneration = currentGeneration;
     }
 
     List<Cell> newGeneration() {
-        List<Cell> newGeneration = new ArrayList<>();
+        List<Cell> newGeneration = new ArrayList<>(currentGeneration);
+        for (Cell cell : currentGeneration) {
+            this.removeDeadCellsFromNewGeneration(newGeneration, cell);
+        }
 
         return newGeneration;
+    }
+
+    private void removeDeadCellsFromNewGeneration(List<Cell> newGeneration, Cell cell) {
+        int liveNeighbours = 0;
+        List<Cell> neighbours = cell.getNeighbours();
+        for (Cell neighbour : neighbours) {
+            if (currentGeneration.contains(neighbour))
+                liveNeighbours++;
+        }
+        if (liveNeighbours < 2)
+            newGeneration.remove(cell);
     }
 }
--- src/test/java/com/tw/assignments/gameoflife/UniverseTest.java
+++ src/test/java/com/tw/assignments/gameoflife/UniverseTest.java
@@ -11,21 +11,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UniverseTest {
 
-    @Test
-    void shouldReturnTrueIfNextGenerationIsEqualToExpected() {
 
+    @Test
+    void expectCellDieByLonelinessWhenAliveNeighbourCellsAreLessThanTwo() {
         List<Cell> currentGeneration = new ArrayList<>();
-        currentGeneration.add(new Cell(1,1));
-        currentGeneration.add(new Cell(1,2));
-        currentGeneration.add(new Cell(2,1));
-        currentGeneration.add(new Cell(2,2));
+        currentGeneration.add(new Cell(1, 1));
+        currentGeneration.add(new Cell(1, 2));
+        currentGeneration.add(new Cell(2, 0));
         Universe universe = new Universe(currentGeneration);
 
         List<Cell> expectedNewGeneration = new ArrayList<>();
-//        expectedNewGeneration.add(new Cell(1,1));
-//        expectedNewGeneration.add(new Cell(1,2));
-//        expectedNewGeneration.add(new Cell(2,1));
-//        expectedNewGeneration.add(new Cell(2,2));
+        expectedNewGeneration.add(new Cell(1, 1));
 
         List<Cell> newGeneration = universe.newGeneration();
         assertThat(expectedNewGeneration, is(equalTo(newGeneration)));
